f0b10052d8f300ec4a1b18d3d9416afa5be5d54c
luckystar20206 <luckystar20206@gmail.com>
2024-03-01 10:46:51 +0700

    menambahkan post ke 2

--- routes/web.php
+++ routes/web.php
@@ -38,7 +38,13 @@ Route::get('/blog', function(){
             "Author"=>"Rangga Saputra",
             "body"=>"Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium quidem, cumque ipsam quam cum, quos expedita neque quod sapiente ullam adipisci provident quasi odio ex eaque a perferendis impedit enim vero doloremque id natus incidunt modi! Suscipit repellendus modi veniam quam et repudiandae officiis, quidem quia distinctio necessitatibus. Recusandae veritatis, sequi debitis non ducimus cupiditate cum atque reiciendis corrupti quibusdam ex. Itaque illum, error similique temporibus, voluptate architecto adipisci sequi, libero porro ab voluptas debitis omnis dolores accusamus iure ducimus."
         ],
-       
+        [
+            "tittle"=>"Judul Post Kedua",
+            "slug"=>"judul-post-kedua",
+            "Author"=>"Book Yourself",
+            "body"=>"Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium quidem, cumque ipsam quam cum, quos expedita neque quod sapiente ullam adipisci provident quasi odio ex eaque a perferendis impedit enim vero doloremque id natus incidunt modi! Suscipit repellendus modi veniam quam et repudiandae officiis, quidem quia distinctio necessitatibus. Recusandae veritatis, sequi debitis non ducimus cupiditate cum atque reiciendis corrupti quibusdam ex. Itaque illum, error similique temporibus, voluptate architecto adipisci sequi, libero porro ab voluptas debitis omnis dolores accusamus iure ducimus."
+        ],
+        
 ];
     return view ('posts', [
         "tittle"=> "Blog",
@@ -54,6 +60,12 @@ Route::get('posts/{slug}', function($slug){
             "tittle"=>"Judul Post Pertama",
             "slug" => "judul-post-pertama",
             "Author"=>"Rangga Saputra",
+            "body"=>"Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium quidem, cumque ipsam quam cum, quos expedita neque quod sapiente ullam adipisci provident quasi odio ex eaque a perferendis impedit enim vero doloremque id natus incidunt modi! Suscipit repellendus modi veniam quam et repudiandae officiis, quidem quia distinctio necessitatibus. Recusandae veritatis, sequi debitis non ducimus cupiditate cum atque reiciendis corrupti quibusdam ex. Itaque illum, error similique temporibus, voluptate architecto adipisci sequi, libero porro ab voluptas debitis omnis dolores accusamus iure ducimus."
+        ],
+        [
+            "tittle"=>"Judul Post Kedua",
+            "slug"=>"judul-post-kedua",
+            "Author"=>"Book Yourself",
             "body"=>"Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium quidem, cumque ipsam quam cum, quos expedita neque quod sapiente ullam adipisci provident quasi odio ex eaque a perferendis impedit enim vero doloremque id natus incidunt modi! Suscipit repellendus modi veniam quam et repudiandae officiis, quidem quia distinctio necessitatibus. Recusandae veritatis, sequi debitis non ducimus cupiditate cum atque reiciendis corrupti quibusdam ex. Itaque illum, error similique temporibus, voluptate architecto adipisci sequi, libero porro ab voluptas debitis omnis dolores accusamus iure ducimus."
         ],
         
