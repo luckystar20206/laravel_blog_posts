afd50671277a3dc7d5033924e1aca3b6275bca82
luckystar20206 <luckystar20206@gmail.com>
2024-03-04 13:17:43 +0700

    memperbaiki kode setelah berhasil melakukan insert data ke database menggunakan mess assigment

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -2,8 +2,8 @@
 
 @section('container')
 
-        <h2>{{ $post->tittle }}</h2>
-        <p>{{ $post->body }}
+    <h2>{{ $post->tittle }}</h2>
+    <p>{{ $post->body }}
     <br><br>
     <a href="/posts">Back to Blog</a>
 @endsection
