a3205c4c8d65d6e5692206e63a286c4ca8d506bc
luckystar20206 <luckystar20206@gmail.com>
2024-03-05 09:15:49 +0700

    menambahkan code untuk melakukan seeder di laravel dan menerapkan perintah tinker di laravel

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
+use App\Models\Post;
+use App\Models\Category;
+
+
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -20,5 +25,44 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        User::create([
+            'name'=>'Rangga Saputra',
+            'email'=>'rangga@gmail.com',
+            'password'=> bcrypt('12345')
+        ]);
+
+        Category::create([
+            'name'=>'Web Programming',
+            'slug'=>'Web-Programming',
+        ]);
+        Category::create([
+            'name'=>'Personal',
+            'slug'=>'personal',
+        ]);
+        Post::create([
+            'tittle'=>'Judul Pertama',
+            'slug'=>'judul-pertama',
+            'excerpt'=>'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam, cum?',
+            'body'=>'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia, cupiditate pariatur sequi laborum nulla, tempore temporibus sit sed voluptates suscipit, necessitatibus explicabo consequuntur quisquam ducimus maiores. Nisi dolores ad ab veniam? Quod officiis magni expedita, velit recusandae eaque illo reiciendis libero architecto repellendus impedit ad dolor eius pariatur in modi quo, asperiores neque. Corporis beatae, quidem nulla vero necessitatibus quaerat ut ratione fuga placeat ea esse deleniti quasi iure laboriosam natus reiciendis? Sed iure velit libero ratione illum, consequuntur accusantium.',
+            'category_id'=> 1,
+            'user_id'=> 1
+        ]);
+        Post::create([
+            'tittle'=>'Judul Kedua',
+            'slug'=>'judul-kedua',
+            'excerpt'=>'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam, cum?',
+            'body'=>'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia, cupiditate pariatur sequi laborum nulla, tempore temporibus sit sed voluptates suscipit, necessitatibus explicabo consequuntur quisquam ducimus maiores. Nisi dolores ad ab veniam? Quod officiis magni expedita, velit recusandae eaque illo reiciendis libero architecto repellendus impedit ad dolor eius pariatur in modi quo, asperiores neque. Corporis beatae, quidem nulla vero necessitatibus quaerat ut ratione fuga placeat ea esse deleniti quasi iure laboriosam natus reiciendis? Sed iure velit libero ratione illum, consequuntur accusantium.',
+            'category_id'=> 1,
+            'user_id'=> 1
+        ]);
+        Post::create([
+            'tittle'=>'Judul Ketiga',
+            'slug'=>'judul-ketiga',
+            'excerpt'=>'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam, cum?',
+            'body'=>'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia, cupiditate pariatur sequi laborum nulla, tempore temporibus sit sed voluptates suscipit, necessitatibus explicabo consequuntur quisquam ducimus maiores. Nisi dolores ad ab veniam? Quod officiis magni expedita, velit recusandae eaque illo reiciendis libero architecto repellendus impedit ad dolor eius pariatur in modi quo, asperiores neque. Corporis beatae, quidem nulla vero necessitatibus quaerat ut ratione fuga placeat ea esse deleniti quasi iure laboriosam natus reiciendis? Sed iure velit libero ratione illum, consequuntur accusantium.',
+            'category_id'=> 2,
+            'user_id'=> 1
+        ]);
     }
 }
