bf247a88ee610a265fd7c29f55caefe3c57bee1e
luckystar20206 <luckystar20206@gmail.com>
2024-03-08 07:54:43 +0700

    menambahkan baris fungsi created_at agar bisa melihat kapan postingan ini di unggah

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -11,8 +11,12 @@
         <img src="..." class="card-img-top" alt="...">
         <div class="card-body">
           <h3 class="card-title">{{ $posts[0]->tittle }}</h3>
-          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-          <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+          <p>
+            <small class="text-body-secondary">
+            By. <a href="/authors/{{ $posts[0]->author->username }}" class="text-decoration-none">{{ $posts[0]->author->name }}</a> In <a href="/categories/{{ $posts[0]->category->slug }}" class="text-decoration-none">{{ $posts[0]->category->name }}</a> {{ $posts[0]->created_at->diffForHumans() }}
+            </small>
+          </p>
+          <p class="card-text">{{ $posts[0]->excerpt }}</p>
         </div>
     </div>
         
