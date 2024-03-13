0e8fa869e690e2837ac92607d2e2af9d813f3f21
luckystar20206 <luckystar20206@gmail.com>
2024-03-13 09:27:13 +0700

    Registration for User Done

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class RegisterController extends Controller
 {
@@ -14,13 +15,18 @@ class RegisterController extends Controller
     }
 
     public function store(Request $request){
-        $request->validate([
+        $validatedData = $request->validate([
             'name' => 'required|max:255',
             'username' => ['required', 'min:3', 'max:255', 'unique:users'],
             'email' => 'required|email:dns|unique:users',
             'password'=> 'required|min:5|max:255'
         ]);
-        dd('registrasi berhasil');
+
+        $validatedData['password'] = bcrypt($validatedData['password']);
+        
+        User::create($validatedData);
+
+        return redirect('/login')->with('success', 'Register Succesfully! Please Login');
 
     }
 }
--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -3,6 +3,16 @@
 @section('container')
 <div class="row justify-content-center">
   <div class="col-lg-4 mt-4">
+
+    {{-- Alert for registration Success --}}
+    @if (session('success'))
+    <div class="alert alert-success alert-dismissible fade show" role="alert">
+      {{ session('success') }}
+      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+    @endif
+    {{-- End Allert --}}
+
     <main class="form-signin w-100 m-auto">
       <h1 class="h3 mb-3 fw-normal text-center">Please Login</h1>
       <form>
