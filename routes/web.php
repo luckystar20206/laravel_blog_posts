7201994fa58530c076416fcc646537ac15e0b6b9
luckystar20206 <luckystar20206@gmail.com>
2024-03-18 12:50:54 +0700

    create dashboard page using bootstrap

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
-use App\Http\Controllers\DashboardController;
 
 
 /*
@@ -66,8 +65,11 @@ Route::post('/logout', [LoginController::class, 'logout']);
 Route::get('/register', [RegisterController::class, 'index'])->middleware('guest');
 Route::post('/register', [RegisterController::class, 'store']);
 
+
 // Route Halaman Dashboard
-Route::get('/dashboard', [DashboardController::class , 'index'])->middleware('auth');
+Route::get('/dashboard', function(){
+    return view ('dashboard.index');
+})->middleware('auth');
 
 
 
