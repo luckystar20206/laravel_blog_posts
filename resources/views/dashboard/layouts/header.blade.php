cce9825e7740b6c078a3b22a87f1c972c9e33b49
luckystar20206 <luckystar20206@gmail.com>
2024-03-18 16:05:43 +0700

    membuat halaman dashboard agar menerapkan blade templating engine laravel

---
+++ resources/views/dashboard/layouts/header.blade.php
@@ -0,0 +1,13 @@
+<header class="navbar sticky-top bg-dark flex-md-nowrap p-0 shadow" data-bs-theme="dark">
+    <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6 text-white" href="#">Rangga Blog Post</a>
+  
+    <ul class="navbar-nav flex-row d-md-none">
+      
+      <li class="nav-item text-nowrap">
+        <button class="nav-link px-3 text-white" type="button" data-bs-toggle="offcanvas" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+          <svg class="bi"><use xlink:href="#list"/></svg>
+        </button>
+      </li>
+    </ul>
+  
+  </header>
---
+++ resources/views/dashboard/layouts/sidebar.blade.php
@@ -0,0 +1,38 @@
+<div class="sidebar border border-right col-md-3 col-lg-2 p-0 bg-body-tertiary">
+    <div class="offcanvas-md offcanvas-end bg-body-tertiary" tabindex="-1" id="sidebarMenu" aria-labelledby="sidebarMenuLabel">
+      <div class="offcanvas-header">
+        <h5 class="offcanvas-title" id="sidebarMenuLabel">Rangga Blog Post</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" data-bs-target="#sidebarMenu" aria-label="Close"></button>
+      </div>
+
+      <div class="offcanvas-body d-md-flex flex-column p-0 pt-lg-3 overflow-y-auto">
+        <ul class="nav flex-column">
+          <li class="nav-item">
+            <a class="nav-link d-flex align-items-center gap-2 active" aria-current="page" href="#">
+              <svg class="bi"><use xlink:href="#house-fill"/></svg>
+              Dashboard
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link d-flex align-items-center gap-2" href="#">
+              <svg class="bi"><use xlink:href="#file-earmark"/></svg>
+              My Posts
+            </a>
+          </li>
+        </ul>
+
+        <hr class="my-3">
+
+        <ul class="nav flex-column mb-auto">
+          <li class="nav-item">
+            <form action="/logout" method="post">
+              @csrf
+              <button type="submit" class="nav-link d-flex align-items-center gap-2"> <svg class="bi"><use xlink:href="#door-closed"/></svg> Logout</a></li></button>
+            </form>
+
+
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
