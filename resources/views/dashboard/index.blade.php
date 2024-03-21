aaca2aafaa312fc8d1e2656f8494c447b5bdec64
luckystar20206 <luckystar20206@gmail.com>
2024-03-21 16:07:41 +0700

    clean codde

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -2,7 +2,10 @@
 
 
 @section('container')
+
+{{-- Main Dashboard --}}
     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
     <h1 class="h2">Welcome Back, {{ auth()->user()->name }}</h1>
     </div>
+{{-- End Main --}}
 @endsection
--- resources/views/dashboard/posts/index.blade.php
+++ resources/views/dashboard/posts/index.blade.php
@@ -2,6 +2,7 @@
 
 
 @section('container')
+  {{-- Main Dashboard Post --}}
     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
         <h1 class="h2">My Post</h1>
     </div>
@@ -44,4 +45,5 @@
           </tbody>
         </table>
       </div>
+    {{-- End Main --}}
 @endsection
