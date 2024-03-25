50234e1b4c3805ebd741f351ea12739fd51603f5
luckystar20206 <luckystar20206@gmail.com>
2024-03-25 12:12:26 +0700

    create form

--- resources/views/dashboard/posts/create.blade.php
+++ resources/views/dashboard/posts/create.blade.php
@@ -6,6 +6,25 @@
     <h1 class="h2">Create New Posts</h1>
 </div>
 
+<div class="col-lg-8">
+    <form method="post" action="/dashboard/posts">
+        @csrf
+        <div class="mb-3">
+          <label for="tittle" class="form-label">Tittle</label>
+          <input type="text" class="form-control" id="tittle" name="tittle">
+        </div>
+        <div class="mb-3">
+          <label for="tittle" class="form-label">Tittle</label>
+          <input type="text" class="form-control" id="tittle" name="tittle">
+        </div>
+        <div class="mb-3">
+          <label for="tittle" class="form-label">Tittle</label>
+          <input type="text" class="form-control" id="tittle" name="tittle">
+        </div>
+       
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+</div>
 
 
 @endsection
