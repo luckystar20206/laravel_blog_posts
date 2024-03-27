e2e74f846f89558457d71a2fd2c3e8d0f9f8b637
luckystar20206 <luckystar20206@gmail.com>
2024-03-27 12:45:21 +0700

    create function for dashboard posts done

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -2,14 +2,14 @@
 
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container">
-      <a class="navbar-brand" href="/">Rangga Saputra</a>
+      <a class="navbar-brand nav-link {{ Request::is('/') ? 'active' : '' }}" href="/">Rangga Saputra</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav">
           <li class="nav-item">
-            <a class="nav-link {{ Request::is('home') ? 'active' : '' }}" href="/">Home</a>
+            <a class="nav-link {{ Request::is('/') ? 'active' : '' }}" href="/">Home</a>
           </li>
           <li class="nav-item">
             <a class="nav-link {{ Request::is('about') ? 'active' : '' }}" href="/about">About</a>
