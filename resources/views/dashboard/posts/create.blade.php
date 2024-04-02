5f7d9cddfa40b586d44dadc654d89ea5e4555e66
luckystar20206 <luckystar20206@gmail.com>
2024-04-02 09:58:03 +0700

    create image preview for dashboard post create page

--- resources/views/dashboard/posts/create.blade.php
+++ resources/views/dashboard/posts/create.blade.php
@@ -44,7 +44,8 @@
 
         <div class="mb-3">
           <label for="image" class="form-label">Post Image</label>
-          <input class="form-control @error('image') is-invalid @enderror" type="file" id="image" name="image">
+          <img class="img-preview img-fluid mb-3 col-sm-5">
+          <input class="form-control @error('image') is-invalid @enderror" type="file" id="image" name="image" onchange="previewImage()">
           @error('image')
           <div class="invalid-feedback">
             {{ $message }}
@@ -82,6 +83,21 @@
     e.preventDefault();
   })
 
+  // Image Preview
+  function previewImage(){
+
+    const image = document.querySelector('#image');
+    const imgPreview = document.querySelector('.img-preview');
+    
+    imgPreview.style.display = "block";
+
+    const oFReader = new FileReader();
+    oFReader.readAsDataURL(image.files[0]);
+    oFReader.onload = function(oFREvent){
+      imgPreview.src = oFREvent.target.result;
+    }
+  }
+    
 </script>
 {{-- End Body --}}
 @endsection
