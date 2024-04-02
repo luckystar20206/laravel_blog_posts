2e7ffa3c893be59f055e06fef0a45eb5976eb68b
luckystar20206 <luckystar20206@gmail.com>
2024-04-02 16:54:58 +0700

    change device

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -3,7 +3,7 @@
     @section('container')
 
     {{-- Body About --}}
-        <h1>Ini Halaman About</h1>
+        <h1>Ini adalah Halaman About</h1>
         <h3> {{ $name }} </h3>
         <p>{{ $email }}</p>
         <img src="img/{{ $image }}" alt="{{ $alt }}" width="200px" class="img-thumbnail rounded-circle">
