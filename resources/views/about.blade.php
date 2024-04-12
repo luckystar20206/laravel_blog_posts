b7c4605b77f97a43518d36b7fdd0a791ea1fd204
luckystar20206 <luckystar20206@gmail.com>
2024-04-12 20:48:51 +0700

    create function with google (proccess)

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -4,7 +4,7 @@
 
     {{-- Body About --}}
         <h1>Ini adalah Halaman About</h1>
-        <h3> {{ $name }} </h3>
+        <h3> {{ $name }}</h3>
         <p>{{ $email }}</p>
         <img src="img/{{ $image }}" alt="{{ $alt }}" width="200px" class="img-thumbnail rounded-circle">
     {{-- End Body --}}
--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -50,8 +50,25 @@
         <button class="btn btn-primary w-100 py-2" type="submit">Login</button>
       </form>
       <small class="d-block text-center mt-3">Not Have Account? <a href="/register"> Register Now!</a></small>
+
+        <div id="g_id_onload"
+          data-client_id="YOUR_GOOGLE_CLIENT_ID"
+          data-login_uri="https://your.domain/your_login_endpoint"
+          data-auto_prompt="false">
+      </div>
+      <div class="g_id_signin"
+          data-type="standard"
+          data-size="large"
+          data-theme="outline"
+          data-text="sign_in_with"
+          data-shape="rectangular"
+          data-logo_alignment="left">
+      </div>
   </main>
   </div>
 </div>
+
+{{-- login With Google --}}
+<script src="https://accounts.google.com/gsi/client" async></script>
 
 @endsection
